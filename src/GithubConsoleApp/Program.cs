4634745efe9f30482a6f79fc48f5ac3b3de6f5bc
jalpesh vadgama <Jalpesh@dotnetjalps.com>
2016-06-27 12:40:35 +0530

    Add project files.

---
+++ src/GithubConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GithubConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Github sample application");
+            Console.ReadLine();
+        }
+    }
+}
